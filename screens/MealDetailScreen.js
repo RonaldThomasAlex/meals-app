d82fac1f3ea70377df392060d10dd3e43cc8e036
Ronald Thomas <ronaldthomas094@gmail.com>
2023-08-19 22:31:59 +0530

    data in meal detail screen

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,8 +1,13 @@
 import { useLayoutEffect } from "react";
-import { Text } from "react-native";
+import { Text, View, Image } from "react-native";
+
+import { MEALS } from "../data/dummy-data";
+import MealDetails from "../components/MealDetails";
 
 const MealDetailScreen = ({ route, navigation }) => {
   const { mealId } = route.params;
+
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -10,7 +15,31 @@ const MealDetailScreen = ({ route, navigation }) => {
     });
   }, [mealId, route]);
 
-  return <Text>Meal detail screen</Text>;
+  return (
+    <View>
+      <Image source={{ uri: selectedMeal.imageUrl }} />
+
+      <Text>{selectedMeal.title}</Text>
+
+      <MealDetails
+        duration={selectedMeal.duration}
+        affordability={selectedMeal.affordability}
+        complexity={selectedMeal.complexity}
+      />
+
+      <Text>Ingredients</Text>
+
+      {selectedMeal.ingredients.map((ingredient) => {
+        return <Text key={ingredient}>{ingredient}</Text>;
+      })}
+
+      <Text>Steps</Text>
+
+      {selectedMeal.steps.map((step) => {
+        return <Text key={step}>{step}</Text>;
+      })}
+    </View>
+  );
 };
 
 export default MealDetailScreen;
